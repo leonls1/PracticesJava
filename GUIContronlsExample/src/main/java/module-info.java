c57a67d1909a15ed7d99bb97ffb9a1594dad3395
leonls1 <leonlederhossturich@gmail.com>
2023-10-06 09:07:56 -0300

    solving problems with javafx version

--- GUIContronlsExample/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module GUIExample {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-    requires java.logging;
-
-    opens GUIExample to javafx.fxml;
-    exports GUIExample.app;
-}
