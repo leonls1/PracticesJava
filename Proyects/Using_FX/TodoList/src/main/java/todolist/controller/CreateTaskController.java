a0688d41e5622a79a8fbd42be37b9fd6f1cf03bf
leonls1 <leonlederhossturich@gmail.com>
2023-10-29 10:04:53 -0300

    methods and nods defined

--- Proyects/Using_FX/TodoList/src/main/java/todolist/controller/CreateTaskController.java
+++ Proyects/Using_FX/TodoList/src/main/java/todolist/controller/CreateTaskController.java
@@ -33,8 +33,8 @@ public class CreateTaskController implements Initializable {
     private ArrayList<Task> tasks;
     private Task task;
 
-    @FXML
-    private Button btnRead, btnReset, btnCreate;
+@FXML
+private Button btnRead, btnReset, btnCreate;
 
     @FXML
     private ComboBox<TaskType> cboType;
--- Proyects/Using_FX/TodoList/src/main/java/todolist/controller/ReadTaskController.java
+++ Proyects/Using_FX/TodoList/src/main/java/todolist/controller/ReadTaskController.java
@@ -6,7 +6,14 @@ package todolist.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -15,12 +22,39 @@ import javafx.fxml.Initializable;
  */
 public class ReadTaskController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private Button btnSearch, btnCreate, btnEdit, btnDelete;
+    
+    @FXML
+    private TextField txtName;
+    
+    @FXML
+    private TextArea txtDescription;
+    
+    @FXML 
+    private CheckBox checkBoxImportant;
+    
+    @FXML
+    private ComboBox cboTypes;
+    
+    @FXML
+    private TableView tableViewTask;
+    
+    @FXML
+    private void comboBoxEvent(){
+        
+    }
+    
+    @FXML
+    private void buttonEvent(){
+        
+    }
+    
+    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+
     }    
     
 }
