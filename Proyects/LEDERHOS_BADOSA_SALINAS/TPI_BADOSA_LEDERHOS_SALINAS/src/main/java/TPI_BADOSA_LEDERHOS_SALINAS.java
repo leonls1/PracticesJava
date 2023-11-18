ac33047be52ffd7c02e102d40766b80a915e814e
leonls1 <leonlederhossturich@gmail.com>
2023-11-18 14:41:19 -0300

    base para el tpi

---
+++ Proyects/LEDERHOS_BADOSA_SALINAS/TPI_BADOSA_LEDERHOS_SALINAS/src/main/java/TPI_BADOSA_LEDERHOS_SALINAS.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author leon
+ */
+public class TPI_BADOSA_LEDERHOS_SALINAS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
