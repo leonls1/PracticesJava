57f429f533b8e76669bfeb7d9c4d6ca252a3e67c
Jonathan Salinas <solojulian01@gmail.com>
2023-11-21 15:54:24 -0300

    SpecialityServices

---
+++ Proyects/LEDERHOS_BADOSA_SALINAS/TPI_BADOSA_LEDERHOS_SALINAS/src/main/java/model/service/SpecialityService.java
@@ -0,0 +1,11 @@
+import java.util.List;
+import model.businessLogic.entity.Speciality;
+
+public interface SpecialityService {
+    public List<Speciality> getAll();
+    public Speciality getById(Long id);
+    public void create(Speciality speciality);
+    public void delete(Long id);
+    public void update(Speciality speciality);
+
+}
