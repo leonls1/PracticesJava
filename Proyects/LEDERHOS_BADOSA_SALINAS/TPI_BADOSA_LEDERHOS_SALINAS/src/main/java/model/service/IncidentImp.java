0ebd6c046639493955db84d93dd68c4b34e24dc9
leonls1 <leonlederhossturich@gmail.com>
2023-11-22 12:26:48 -0300

    n

--- Proyects/LEDERHOS_BADOSA_SALINAS/TPI_BADOSA_LEDERHOS_SALINAS/src/main/java/model/service/IncidentImp.java
+++ Proyects/LEDERHOS_BADOSA_SALINAS/TPI_BADOSA_LEDERHOS_SALINAS/src/main/java/model/service/IncidentImp.java
@@ -4,12 +4,15 @@
  */
 package model.service;
 
+import java.util.Calendar;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import model.businessLogic.entity.Incident;
 import model.persistence.IncidentRepository;
 import org.springframework.stereotype.Service;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 @Service
 public class IncidentImp implements IncidentService{
@@ -55,8 +58,30 @@ public class IncidentImp implements IncidentService{
         
     }
 
+    
+    private  boolean esAntesDeNDias(Date fecha, int dias) {
+        // Obtener la fecha actual
+        Calendar fechaActual = Calendar.getInstance();
+
+        // Restar n días a la fecha actual
+        fechaActual.add(Calendar.DAY_OF_MONTH, -dias);
+
+        // Crear un objeto Calendar para la fecha dada
+        Calendar fechaDada = Calendar.getInstance();
+        fechaDada.setTime(fecha);
+
+        // Comparar las fechas
+        return fechaDada.before(fechaActual.getTime());
+    }
+    
     @Override
-    public List<Incident> getIncidentsLastDays() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public List<Incident> getIncidentsLastDays(int nDays) {
+        
+        List<Incident> list = getAll().stream()
+                .filter(incident -> esAntesDeNDias(incident.getRealDate(), nDays) )
+                .collect(Collectors.toList());
+        
+        
+        return list;
     }
 }
