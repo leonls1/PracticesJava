8456bb6a696b2f8fcd81328ef2c197d5549ccfed
leonls1 <leonlederhossturich@gmail.com>
2023-11-25 12:13:47 -0300

    25/11

---
+++ Proyects/LEDERHOS_BADOSA_SALINAS/TPI_V2/src/main/java/tpi_v2/model/businessLogic/entity/Specialty.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tpi_v2.model.businessLogic.entity;
+
+/**
+ *
+ * @author leon
+ */
+public class Specialty {
+    
+}
