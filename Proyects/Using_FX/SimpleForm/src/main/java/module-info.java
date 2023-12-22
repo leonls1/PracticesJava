091aa010cd4cc3f6d9403297352f80289492fca1
leonls1 <leonlederhossturich@gmail.com>
2023-12-22 09:23:08 -0300

    22/12

--- Proyects/Using_FX/SimpleForm/src/main/java/module-info.java
+++ Proyects/Using_FX/SimpleForm/src/main/java/module-info.java
@@ -2,10 +2,12 @@ module form {
     requires javafx.controls;
     requires javafx.fxml;
     requires jakarta.persistence;
+    requires java.base;
     //requires lombok;
     
     opens form to javafx.fxml;
     opens form.controller;
+    opens form.entity;
     
     exports form;
     exports form.controller;
