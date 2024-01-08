759d380251945b6c3e380c6ea46f0fca4acad477
leonls1 <leonlederhossturich@gmail.com>
2024-01-08 19:37:42 -0300

    44/90 cap 5

--- Proyects/Using_FX/JavaFXCrud/src/main/java/form/javafxcrud/controller/StudentController.java
+++ Proyects/Using_FX/JavaFXCrud/src/main/java/form/javafxcrud/controller/StudentController.java
@@ -19,105 +19,124 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 
-
 public class StudentController implements Initializable {
 
     private final AppQuery query = new AppQuery();
-       
+
     @FXML
     public TextField fieldFirstName;
-    
+
     @FXML
     public TextField fieldMiddleName;
-    
+
     @FXML
     public TextField fieldSurname;
-    
+
     @FXML
     public Button btnNew;
-    
+
     @FXML
     public Button btnUpdate;
-    
+
     @FXML
     public Button btnSave;
-    
+
     @FXML
     public Button btnDelete;
-    
+
     @FXML
     public TableView<Student> tableView;
-    
+
     @FXML
-    public TableColumn<Student, Integer> colId ;
-    
+    public TableColumn<Student, Integer> colId;
+
     @FXML
-    public TableColumn<Student, String> colFirtsName;  
-    
-    @FXML    
-    public TableColumn<Student, String> colMiddleName;  
-    
+    public TableColumn<Student, String> colFirtsName;
+
     @FXML
-    public TableColumn<Student, String> colSurname;  
-    
+    public TableColumn<Student, String> colMiddleName;
+
+    @FXML
+    public TableColumn<Student, String> colSurname;
+
     private Student student;
-            
+
     @FXML
-    private void addStudent(){
+    private void addStudent() {
         Student stu = new Student(
                 fieldFirstName.getText(),
                 fieldMiddleName.getText(),
                 fieldSurname.getText());
-        
+
         query.addStudent(stu);
+        getStudents();
+        System.out.println("Student added");
     }
-    
+
     @FXML
-    private void btnEvent(ActionEvent e){
-        Object evt = e.getSource();
-        
-        if(evt.equals(btnDelete)){
-            
-        }
-        
-    }
-    
-    @FXML
-    private void mouseClicked(MouseEvent mouseEvent){
-        //Object evt = e.getSource();
-        
+    private void updateStudent() {
         try {
-            Student stu = tableView.getSelectionModel().getSelectedItem();
-            student = stu;
-            fieldFirstName.setText(student.getFirstName());
-            fieldMiddleName.setText(student.getMiddleName());
-            fieldSurname.setText(student.getSurname());
-            
-            btnDelete.setDisable(false);
-            btnUpdate.setDisable(false);
-            btnSave.setDisable(true);
-            
+            query.updateStudent(tableView.getSelectionModel().getSelectedItem());
+            tableView.refresh();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void deleteStudent() {
+        try {
+            query.removeStudent(tableView.getSelectionModel().getSelectedItem().getId());
+            getStudents();
         } catch (Exception e) {
         }
     }
-    
+
     @FXML
-    private void getStudents(){
+    private void newStudent() {
+        btnDelete.setDisable(true);
+        btnUpdate.setDisable(true);
+        btnSave.setDisable(false);
+
+        fieldFirstName.setText("");
+        fieldMiddleName.setText("");
+        fieldSurname.setText("");
+    }
+
+    @FXML
+    private void mouseClicked(MouseEvent mouseEvent) {
+        //Object evt = e.getSource();
+
+        try {
+            Student stu = tableView.getSelectionModel().getSelectedItem();
+            this.student = stu;
+            fieldFirstName.setText(student.getFirstName());
+            fieldMiddleName.setText(student.getMiddleName());
+            fieldSurname.setText(student.getSurname());
+
+            btnDelete.setDisable(false);
+            btnUpdate.setDisable(false);
+            btnSave.setDisable(true);
+
+        } catch (Exception e) {
+        }
+    }
+
+    @FXML
+    private void getStudents() {
         ObservableList list = query.getStudents();
-        
+
         colId.setCellValueFactory(new PropertyValueFactory<>("id"));
         colFirtsName.setCellValueFactory(new PropertyValueFactory<>("firstName"));
         colMiddleName.setCellValueFactory(new PropertyValueFactory<>("middleName"));
         colSurname.setCellValueFactory(new PropertyValueFactory<>("surname"));
-        
-        
+
         tableView.setItems(list);
     }
-    
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         getStudents();
-    }    
-    
+    }
+
 }
--- Proyects/Using_FX/JavaFXCrud/src/main/java/form/javafxcrud/data/AppQuery.java
+++ Proyects/Using_FX/JavaFXCrud/src/main/java/form/javafxcrud/data/AppQuery.java
@@ -11,62 +11,93 @@ import java.sql.ResultSet;
 public class AppQuery {
 
     private DBConnection db = new DBConnection();
-    
-    public void addStudent(Student student){
+
+    public void addStudent(Student student) {
         try {
             db.getDBConection();
-            PreparedStatement ps = db.getCon()                   
-                    .prepareStatement
-            ("INSERT into person(first_name, middle_name, surname) values(?,?,?)");
-            
+            PreparedStatement ps = db.getCon()
+                    .prepareStatement("INSERT into person(first_name, middle_name, surname) values(?,?,?)");
+
             ps.setString(1, student.getFirstName());
             ps.setString(2, student.getMiddleName());
             ps.setString(3, student.getSurname());
             ps.execute();
             ps.close();
-            
+
             db.closeConn();
-            
-            
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        
+
     }
-    
-    public void removeStudent(int id){
-        String query = "remove from human h where h.id = ?";
-    }
-    
-    public ObservableList<Student> getStudents(){
-        ObservableList<Student> studentList = FXCollections.observableArrayList();
-        
+
+    public void removeStudent(int id) {
         try {
+            db.getDBConection();
+            PreparedStatement ps = db.getCon().prepareStatement(" DELETE FROM `form1`.`person`\n"
+                    + "WHERE id = ?");
+
+            ps.setInt(1, id);
+            ps.execute();
+            ps.close();
+            db.closeConn();
+        } catch (Exception e) {
+        }
+    }
+
+    public void updateStudent(Student student) {
+        try {
+            db.getDBConection();
+            PreparedStatement ps = db.getCon().prepareStatement("UPDATE `form1`.`person`\n"
+                    + "SET\n"
+                    + "`first_name` = ?,\n"
+                    + "`middle_name` = ?,\n"
+                    + "`surname` = ?\n"
+                    + "WHERE `id` = ?");
+            ps.setString(1, student.getFirstName());
+            ps.setString(2, student.getMiddleName());
+            ps.setString(3, student.getSurname());
+            ps.setInt(4, student.getId());
+            ps.execute();
             
+            ps.close();
+            db.closeConn();
+            System.out.println("updated");
+
+        } catch (Exception e) {
+        }
+    }
+
+    public ObservableList<Student> getStudents() {
+        ObservableList<Student> studentList = FXCollections.observableArrayList();
+
+        try {
+
             String query = "SELECT * FROM person order by id";
             db.getDBConection();
             Statement st = db.getCon().createStatement();
             ResultSet rs = st.executeQuery(query);
-            Student stu ;
-            
-            while(rs.next()){
+            Student stu;
+
+            while (rs.next()) {
                 stu = new Student(
-                rs.getInt("id"),
-                rs.getString("first_name"),
-                rs.getString("middle_name"),
-                rs.getString("surname"));
-                
+                        rs.getInt("id"),
+                        rs.getString("first_name"),
+                        rs.getString("middle_name"),
+                        rs.getString("surname"));
+
                 studentList.add(stu);
             }
-            
+
             rs.close();
             st.close();
-            db.closeConn();      
-            
+            db.closeConn();
+
         } catch (Exception e) {
         }
-        
+
         return studentList;
     }
-    
+
 }
