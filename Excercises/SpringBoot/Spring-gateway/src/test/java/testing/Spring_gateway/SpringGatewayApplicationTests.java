b5c1783128d08e3be845f2ca498b14802179e038
leonls1 <leonlederhossturich@gmail.com>
2024-07-22 10:31:44 -0300

    practicing gateway

---
+++ Excercises/SpringBoot/Spring-gateway/src/test/java/testing/Spring_gateway/SpringGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package testing.Spring_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
