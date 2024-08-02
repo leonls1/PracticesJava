2a81440d7ca04b936c7151d5e0106b5d85a3b6b2
leonls1 <leonlederhossturich@gmail.com>
2024-08-02 11:22:22 -0300

    practicing security

---
+++ Excercises/SpringBoot/security/src/main/java/spring_security_JWT/security/configuration/DatabaseConfig.java
@@ -0,0 +1,31 @@
+package spring_security_JWT.security.configuration;
+
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Value("{spring.datasource.url}")
+    private String urlDB;
+
+    @Value("{spring.datasource.username}")
+    private String user;
+
+    @Value("{spring.datasource.password}")
+    private String password;
+
+    @Value("{spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Bean
+    public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(urlDB, user, password);
+        dataSource.setDriverClassName(driverClassName);
+        return dataSource;
+
+    }
+}
---
+++ Excercises/SpringBoot/security/src/test/java/spring_security_JWT/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_security_JWT.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
