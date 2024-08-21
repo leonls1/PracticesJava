c78c9826479babee41e85d543117db366b522216
leonls1 <leonlederhossturich@gmail.com>
2024-08-20 21:28:57 -0300

    20/08

--- Excercises/SpringBoot/security/src/main/java/spring_security_JWT/security/configuration/SecurityConfig.java
+++ Excercises/SpringBoot/security/src/main/java/spring_security_JWT/security/configuration/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.csrf( csrf -> csrf.disable())
-                .securityMatcher(request -> request.)
+                .securityMatcher()
                 .exceptionHandling()
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint)
                 .and()
